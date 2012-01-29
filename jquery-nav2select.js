0aa034c0147ba9c48ef2174811c694e64db73eb4
danieliser <danieliser@wizardinternetsolutions.com>
2012-01-29 02:45:44 -0500

    Update jquery-nav2select.js

---
+++ jquery-nav2select.js
@@ -0,0 +1,46 @@
+(function($){
+  var methods = {
+		init : function( options ) {
+			var opts = $.extend({}, $.fn.nav2select.defaults, options);
+			return this.each(function() {
+				$this = $(this);
+				$select = $("<select />");
+				// Create default option "Go to..."
+				$("<option />", {
+				   "selected": "selected",
+				   "value"   : "",
+				   "text"    : "Go to..."
+				}).appendTo($select);
+				// Populate dropdown with menu items
+				$this.find('a').each(function() {
+					 var el = $(this);
+					 $("<option />", {
+					     "selected": el.attr("href") === window.location.href ? "selected" : false,
+						 "value"   : el.attr("href"),
+						 "text"    : el.text()
+					 }).appendTo($select);
+				});
+				$select.appendTo($this);
+				$($select).change(function() {
+				  window.location = $(this).find("option:selected").val();
+				});	// add all your scripts here
+			});
+		},
+	}
+
+	
+	$.fn.nav2select = function(method) {
+	// Method calling logic
+		if ( methods[method] ) {
+		  return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+		  return methods.init.apply( this, arguments );
+		} else {
+		  $.error( 'Method ' +  method + ' does not exist on jQuery.button' );
+		}
+	
+	};
+	$.fn.nav2select.defaults = {
+		
+	};
+})(jQuery);
